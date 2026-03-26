44786a94fad0386781e4877e4b51351ff0b83869
fbeljdid2 <fbeljdid2@gmail.com>
2026-03-26 02:46:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "🚀 Le bot de Said est enfin EN LIGNE et pret !"
+    return "Bot coteetsport OK"
 
-@app.route('/test-clic')
-def test():
-    return "✅ Le bot arrive a lire cette page !"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route("/test-clic")
+def test_clic():
+    return "Route test-clic OK"
