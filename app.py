46526ce74a6fe9955517a940248bb82867e4c189
fbeljdid2 <fbeljdid2@gmail.com>
2026-03-26 02:06:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Le bot de Said est enfin EN LIGNE et prêt !"
+    return "🚀 Le bot de Said est enfin EN LIGNE et pret !"
 
 @app.route('/test-clic')
 def test():
-    return "✅ Le bot arrive à lire cette page !"
+    return "✅ Le bot arrive a lire cette page !"
 
 if __name__ == "__main__":
-    # Railway utilise une variable d'environnement pour le PORT
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
